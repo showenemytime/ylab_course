91bfadd510010480a8867cc72b54bf094df13373
showenemytime <az.showtime@yandex.ru>
2022-07-01 13:12:40 +0700

    t_2 done without Generator.

--- HW_2/t_2.py
+++ HW_2/t_2.py
@@ -1,36 +1,39 @@
-import datetime
+
 from dataclasses import dataclass
 from datetime import datetime, timedelta
 from typing import Generator, List, Tuple
 
-#
-# @dataclass
-# class Movie:
-#     title: str
-#     dates: List[Tuple[datetime, datetime]]
-#
-#     def schedule(self) -> Generator[datetime, None, None]:
-#         return []
-#
-#
-# m = Movie('sw', [
-#   (datetime(2020, 1, 1), datetime(2020, 1, 7)),
-#   (datetime(2020, 1, 15), datetime(2020, 2, 7))
-# ])
-#
-# for d in m.schedule():
-#     print(d)
 
-m = [
-    (datetime(2020, 1, 1), datetime(2020, 1, 7)),
-    (datetime(2020, 1, 15), datetime(2020, 2, 7)),
-    (datetime(2020, 2, 15), datetime(2020, 2, 18))
- ]
+@dataclass
+class Movie:
+    title: str
+    dates: List[Tuple[datetime, datetime]]
+
+
+    def schedule(self) -> List[List[datetime]]:  #Generator[datetime, None, None]:
+        dates: List[Tuple[datetime, datetime]] = self.dates
+
+        return [[date1 + timedelta(i) for i in range((date2 - date1).days + 1)] for date1, date2 in dates]
 
 
 
 
-for date in m:
-    [print(date[0] + timedelta(i)) for i in range(((date[1] - date[0]).days)+ 1)]
+m = Movie('sw', [
+  (datetime(2020, 1, 1), datetime(2020, 1, 7)),
+  (datetime(2020, 1, 15), datetime(2020, 2, 7))
+])
+
+for date in m.schedule():
+    print(*date, sep='\n')
+
+
+# m = [
+#     (datetime(2020, 1, 1), datetime(2020, 1, 7)),
+#     (datetime(2020, 1, 15), datetime(2020, 2, 7)),
+#  ]
+#
+
+# for date in m:
+#     [print(date[0] + timedelta(i)) for i in range(((date[1] - date[0]).days)+ 1)]
 
 
