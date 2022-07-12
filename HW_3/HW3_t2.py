a47644ace767dee986bc283be3e449e1c779c2fe
showenemytime <az.showtime@yandex.ru>
2022-07-12 16:55:33 +0700

    complited HW2_t2 and added solid_code folder

--- HW_3/HW3_t2.py
+++ HW_3/HW3_t2.py
@@ -1,5 +1,5 @@
 import time
-from typing import Callable
+from typing import Callable, Any
 
 '''  
   Функция декоратор, повторяет основную функцию определенное количество раз с задержкой, которая также растёт
@@ -10,38 +10,45 @@ from typing import Callable
 '''
 
 
-def repeat_func(func) -> Callable[[int, int, int, int], None]:
+def repeat_func(call_count=3, start_sleep_time=1, factor=2, border_sleep_time=15) -> \
+        Callable[[Any], Callable[[], None]]:
+    t = start_sleep_time
 
-    def wrapper(call_count=4, start_sleep_time=1, factor=2, border_sleep_time=15) -> None:
-        print(f'Количество запусков: {call_count}')
-        print(f'Начало работы.\n')
-        call_count_counter: int = 1  # Счётчик повторений цикла.
+    def decorator(func):
+        def wrapper() -> None:
+            sleep_time: int = t
+            print(f'Количество запусков: {call_count}')
+            print(f'Начало работы.\n')
+            call_count_counter: int = 1  # Счётчик повторений цикла.
 
-        #  Цикл, в котором увеличиваем задержку по формуле.
-        while start_sleep_time <= border_sleep_time and call_count >= call_count_counter:
-            time.sleep(start_sleep_time)
+            #  Цикл, в котором увеличиваем задержку по формуле.
+            while sleep_time <= border_sleep_time and call_count >= call_count_counter:
+                time.sleep(sleep_time)
 
-            # Условия для выбора окончания в слове "секудна".
-            if start_sleep_time == 1:
-                sec: str = 'секунда'
-            elif start_sleep_time in (2, 3, 4):
-                sec: str = 'секунды'
-            else:
-                sec: str = 'секунд'
+                # Условия для выбора окончания в слове "секудна".
+                if sleep_time == 1:
+                    sec: str = 'секунда'
+                elif sleep_time in (2, 3, 4):
+                    sec: str = 'секунды'
+                else:
+                    sec: str = 'секунд'
 
-            print(f'Запуск номер {call_count_counter}. Ожидание: {start_sleep_time} {sec}. Результат = {func()}')
-            call_count_counter += 1
-            if start_sleep_time < border_sleep_time:
-                start_sleep_time = start_sleep_time * 2 ** factor
-            if start_sleep_time > border_sleep_time:
-                start_sleep_time = border_sleep_time
-        return None
-    return wrapper
+                print(f'Запуск номер {call_count_counter}. Ожидание: {sleep_time} {sec}. Результат = {func()}')
+                call_count_counter += 1
+                if sleep_time < border_sleep_time:
+                    sleep_time = sleep_time * 2 ** factor
+                if sleep_time > border_sleep_time:
+                    sleep_time = border_sleep_time
+            return None
+
+        return wrapper
+
+    return decorator
 
 
-@repeat_func
+@repeat_func(call_count=4)
 def func_result() -> str:
-    txt: str = 'Я внешняя функция и до сих пор работаю'
+    txt: str = 'Я внешняя функция и до сих пор работаю.'
     return txt
 
 
